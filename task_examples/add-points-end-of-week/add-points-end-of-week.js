7e6c5c8990063cfd0370a1bd035d0538675fd748
confused_techie <dev@lhbasics.com>
2023-11-06 08:26:48 -0800

    Add bonus points at end of week

--- task_examples/add-points-end-of-week/add-points-end-of-week.js
+++ task_examples/add-points-end-of-week/add-points-end-of-week.js
@@ -1,9 +1,11 @@
-const POINT_COUNT = 1;
-const POINT_REASON = "Daily Bonus";
+const POINT_COUNT = 2;
+const POINT_REASON = "Weekly Attendance Bonus";
 
 module.exports =
 async function main(context) {
-  const todaysDate = new Date().toISOString(); // Using ISO string so it's format works with Postgresql
+  let tmpDate = new Date();
+  const lastWeekDate = tmpDate.setDate(tmpDate.getDate() - 7);
+
   const allStudents = await context.database.getAllStudentIDs();
 
   if (!allStudents.ok) {
@@ -12,15 +14,14 @@ async function main(context) {
   }
 
   for (let i = 0; i < allStudents.content.length; i++) {
-
-    // Prior to adding points to the student, lets make sure they were not absent today.
-    const studentHistory = await context.database.getPointsByStudentIDByDate(allStudents.content[i].student_id, todaysDate);
+    const studentHistory = await context.database.getPointsByStudentIDByDate(allStudents.content[i].student_id, lastWeekDate);
 
     if (!studentHistory.ok) {
-      console.error(`Failed to get previous point history for student!`);
+      console.error("Failed to get previousn point history for student!");
       console.error(studentHistory);
       continue;
     }
+
     for (let y = 0; y < studentHistory.content.length; y++) {
       let reason = studentHistory.content[y].reason;
       if (reason.includes("Tardy") || reason.includes("Absent")) {
@@ -30,13 +31,11 @@ async function main(context) {
 
     const addPoints = await context.database.addPointsToStudent(allStudents.content[i].student_id, POINT_COUNT, POINT_REASON);
 
-
     if (!addPoints.ok) {
       console.error(`Failed to add points to ${allStudents.content[i].student_id}! Will keep trying the others`);
       console.error(addPoints);
-      // Then we don't throw here, because we want to keep trying the other users.
     }
   }
 
-  console.log("Done adding the daily bonus of points.");
+  console.log("Done adding extra points.");
 }
